4c63cf141b5bd495030fb9358bb882f048c8ce2c
sagar-374 <sagarkate374@gmail.com>
2026-01-01 19:09:34 +0530

    checkNumber.java file is created

---
+++ checkNumber.java
@@ -0,0 +1,10 @@
+public class checkNumber {
+    public static void main(String[] args) {
+        int a = 5;
+        if(a >= 0) {
+            System.out.println("Number is Positive");
+        } else {
+            System.out.println("Number is Negative");
+        }
+    }
+}
