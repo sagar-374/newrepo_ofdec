9a1166210bb767fc69a8ff9416a81dec72be173f
sagar-374 <sagarkate374@gmail.com>
2026-01-01 13:32:42 +0530

    Updated checkNumber.java file

--- checkNumber.java
+++ checkNumber.java
@@ -2,10 +2,10 @@ public class checkNumber{
    public static void main(){ 
        int a=5;
        if(a%2==0){
-    System.out.println("Number is positive");
+    System.out.println("Number is Even");
 } 
 else { 
-     System.out.println("Number is positive");
+     System.out.println("Number is Odd");
      }
   }
 }
